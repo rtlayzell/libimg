ce503c78ff7d2d6d82003776112324cf0d2ace15
Reegan Layzell <reegan.layzell@live.co.uk>
2014-11-30 11:45:08 +0000

    Added defines _CRT_SECURE_NO_WARNINGS and _SCL_SECURE_NO_WARNINGS

--- libimg/include/stdafx.h
+++ libimg/include/stdafx.h
@@ -7,6 +7,8 @@
 
 #include "targetver.h"
 
+#define _CRT_SECURE_NO_WARNINGS
+#define _SCL_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 
